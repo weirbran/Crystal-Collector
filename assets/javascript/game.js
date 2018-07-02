93cd9e8fd24fc93d7ca7d378cac7c2d6725fb7f3
Brandon Weir <brandon.weir@alumni.utoronto.ca>
2018-07-02 13:08:47 -0400

    JS complete - new functions added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,7 @@
-//GLOBAL VARIABLES 
-
 var wins = 0;
 var losses = 0;
-var computerNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-var totalScore = 0;
+var compScore = 0;
+var userScore = 0;
 var crystalNumber = 0;
 var crystal1 = [];
 var crystal2 = [];
@@ -11,95 +9,63 @@ var crystal3 = [];
 var crystal4 = [];
 
 
+function startGame() {
+
+  userScore = 0;
+
+  function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  compScore = randomNumber(19, 120);
+  crystal1[0] = randomNumber(1, 12);
+  crystal2[0] = randomNumber(1, 12);
+  crystal3[0] = randomNumber(1, 12);
+  crystal4[0] = randomNumber(1, 12);
 
 
-//FUNCTIONS 
-
-//this prints the computer's number in the window 
-$("#compNum").text(computerNumber);
-$("#totalScore").text(totalScore);
-
-
-function randomNumber() {
-  crystalNumber = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-  return crystalNumber;
+  $("#compScore").text(compScore);
+  $("#userScore").text(userScore);
 };
 
-crystal1[0] = randomNumber();
-crystal2[0] = randomNumber();
-crystal3[0] = randomNumber();
-crystal4[0] = randomNumber();
+startGame();
 
-
-function reset() {
-  totalScore = 0;
-  $("#totalScore").text(totalScore);
-  crystal1[0] = randomNumber();
-  crystal2[0] = randomNumber();
-  crystal3[0] = randomNumber();
-  crystal4[0] = randomNumber();
-  computerNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-  $("#compNum").text(computerNumber);
-}
-
-function compareNum() {
-  if (totalScore === computerNumber) {
+function compareScore() {
+  if (userScore === compScore) {
     wins++;
     $("#wins").text(wins);
-    reset();
-  } else if (totalScore > computerNumber) {
+    startGame();
+  } else if (userScore > compScore) {
     losses++;
     $("#losses").text(losses);
-    reset();
+    startGame();
   }
 }
 
+function add(userScore) {
+  $("#userScore").text(userScore);
+}
 
 $("#crystal1").on("click", function () {
-  totalScore = (crystal1[0]) + (totalScore);
-  $("#totalScore").text(totalScore);
-  compareNum();
+  userScore = (crystal1[0]) + (userScore);
+  add(userScore);
+  compareScore();
 });
 
 $("#crystal2").on("click", function () {
-  totalScore = (crystal2[0]) + (totalScore);
-  $("#totalScore").text(totalScore);
-  compareNum();
+  userScore = (crystal2[0]) + (userScore);
+  add(userScore);
+  compareScore();
 });
 
 $("#crystal3").on("click", function () {
-  totalScore = (crystal3[0]) + (totalScore);
-  $("#totalScore").text(totalScore);
-  compareNum();
+  userScore = (crystal3[0]) + (userScore);
+  add(userScore);
+  compareScore();
 });
 
 $("#crystal4").on("click", function () {
-  totalScore = (crystal4[0]) + (totalScore);
-  $("#totalScore").text(totalScore);
-  compareNum();
+  userScore = (crystal4[0]) + (userScore);
+  add(userScore);
+  compareScore();
 });
-
-
-
-
-// function restart() {
-//   $("#compNum").text(computerNumber);
-
-//   function randomNumber() {
-//     crystalNumber = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-//     return crystalNumber;
-//   };
-
-//   crystal1[0] = randomNumber();
-//   console.log(crystal1[0]);
-//   crystal2[0] = randomNumber();
-//   console.log(crystal2[0]);
-//   crystal3[0] = randomNumber();
-//   console.log(crystal3[0]);
-//   crystal4[0] = randomNumber();
-//   console.log(crystal4[0]);
-// }
-
-
-
-
